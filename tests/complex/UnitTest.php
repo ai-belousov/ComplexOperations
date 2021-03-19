56c76e9b6e197b7223918e4410e90abb6ebc604d
Alex <alex-spar-white@mail.ru>
2021-03-19 11:30:46 +0300

    initialozation PhpUnit

---
+++ tests/complex/UnitTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace UnitTest;
+use PHPUnit\Framework\TestCase;
+use App\Complex;
+
+class Test extends TestCase {
+	public function testComplex()
+	{
+		$this->assertEquals(1, 1);
+	}
+
+	protected function setUp() : void
+	{
+		# code...
+	}
+	
+	protected function tearDown() : void
+	{
+		# code...
+	}
+}
